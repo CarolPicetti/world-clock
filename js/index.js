16f5bf9361ebccef003f992a2a8e62f3a14bacc7
Carolina Picetti <carolinapicetti@gmail.com>
2025-11-07 16:56:33 +0100

    Added one more city

--- js/index.js
+++ js/index.js
@@ -17,7 +17,19 @@ let parisTimeElement = parisElement.querySelector(".time");
 let parisTime = moment().tz("Europe/Paris");
 parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
 parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
-}};
+};
+
+//Tokyo
+let tokyoElement = document.querySelector("#tokyo");
+if (tokyoElement){
+let tokyoDateElement = tokyoElement.querySelector(".date");
+let tokyoTimeElement = tokyoElement.querySelector(".time");
+let tokyoTime = moment().tz("Asia/Tokyo");
+tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+};
+}
+
 
 updateTime();
 setInterval(updateTime, 1000);
